ff3786b88bd76d06f84e5cbc871a3c0597983fd7
cliffordEmmanuel <cliffemmanuels@gmail.com>
2020-01-22 16:58:04 +0000

    makes sure no duplicate urls will be saved

--- url_shortner.py
+++ url_shortner.py
@@ -1,20 +1,30 @@
 # Project: URL Shortner
 # Azubi Africa Cohort 1- Getinnotized
-# Date: Friday 17th January, 2020.
-# Written by: Vanessa Quartey and Clifford E. Akai-Nettey
 
 import random
 import csv
+import json
 
 # accept the url
-url = input("Enter your url: ")
 
+
+# so the challenge is to make sure that the script doesn't accept the same url
 # shorten it
 def shorten(url):
     short_url = 'https://' + 'short.lnk/'+ rand_gen() 
-    return [ url, short_url]
+    return (url ,short_url )
+    
 
-ch_list = [ chr(i) for i in range(33,127)]  #generate the ascii characters
+def url_exists(url):
+    # open the file and check if the long url has already been processed
+    with open('links.csv', mode='r') as l_file:
+        reader = csv.reader(l_file)
+        for row in reader:
+            if row[0] == url:
+                return True
+
+
+ch_list = [chr(i) for i in range(48,127)]  #generate the ascii characters
 
 # to make sure shortened links don't repeat.. 
 def rand_gen():
@@ -24,15 +34,14 @@ def rand_gen():
 
 # appending to a csv file
 
-link_pair = shorten(url)
-
-with open('links.csv', mode='a') as data:
-    write = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    write.writerow(link_pair)
-
-data.close()
+def to_file(link_dict):
+    with open('links.csv','a', newline= '') as f:
+        write = csv.writer(f)
+        write.writerow(link_dict)
+    f.close()
 
 
 # this is the entry point for the program
-if __name__ == "__main__":
+if __name__ == "__main__":  
     pass
+
